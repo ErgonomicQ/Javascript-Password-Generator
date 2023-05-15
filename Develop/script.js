f862eb1092ac17ea86646aa622916726c13a375e
ErgonomicQ <ergonomicQ@gmail.com>
2023-05-15 16:11:27 -0700

    readme created, code clean up, comments added

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,6 @@
 
 var generateBtn = document.querySelector("#generate");
 
-// console.log(specialCharacters)
-// console.log(upperCase)
-// console.log(lowerCase)
-// console.log (Numbers)
-// console.log (allOptions)
-
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,37 +12,38 @@ function writePassword() {
   function generatePassword() {
 
 
-//Empty Array for later use
+//Empty Array for later use. Arrays could be used local, but I've chosen to leave them global. 
 var resultsArray = [];
 var userresultArray = [];
 
-    //user is first prompted to select an amount of characters within a predetermined range
+    //user is first prompted to select an amount of characters within a predetermined range. parseInt is used to turn the users string into a number
+    //if the user input could be interpreted as a number. "9" would be interpreted as 9. "C" is not a number, and will not be parsed.
+
    var length = parseInt(prompt("how many characters is your password? Chose Between 8 and 128." , "0"));
 
     // Validator to determine if number length is within the allowed range. 
-
    if (isNaN(length) || length < 8 || length > 128) {
     alert("Invalid Password Length: Click generate again then choose a length between 8 and 128.");
     return generatePassword();
-  
    }
 
-
+//console.log to confirm number was captured through parseInt
    console.log(length)
 
-
+//Confirm Booleans
    var upCase = confirm("Would you like uppercase letters?");
    var lowCase = confirm("would you like lowercase letters?");
    var specialCase = confirm("would you like special characters?");
    var numberCase = confirm("Would you like Numbers as well?");
 
+   // console.log to verify
    console.log(upCase)
    console.log(lowCase)
    console.log(specialCase)
    console.log(numberCase) 
 
 
-   // This confirms if at least ONE chatacter type is selected, and if it is not, it returns the user to the beginning of the prompts.
+   // This confirms if at least ONE character type is selected, and if it is not, it returns the user to the beginning of the prompts.
    if (!upCase && !lowCase && !numberCase && !specialCase){
     alert( "At least one character type must be selected. Please choose a at least one character type.");
     return generatePassword();
@@ -65,7 +58,8 @@ var Numbers= ['1','2','3','4','5','6','7','8','9','0'];
 
 
 
-   //this adds pools based on the confirm prompt returning true or not. resultsArray variable slowly fills up with strings of characters.
+   //this adds character arrays based on the confirm prompt returning true or not. 
+   //resultsArray variable slowly fills up with strings of characters.
   
    if (upCase) {
     resultsArray = resultsArray.concat(upperCase);
@@ -91,7 +85,8 @@ var Numbers= ['1','2','3','4','5','6','7','8','9','0'];
   
   }
 
-
+//Bonus: an Alert has been added to the code to display the password for the user right away, which when closed will be filled into the textbox.
+//Client can leave this on, or can ask to have it commented out at a later date. 
  alert("Your New Password is " + password + ". Don't lose it!");
 }
 
---
+++ README.md
@@ -0,0 +1,52 @@
+# Javascript Password Generator
+
+## Description
+This is a Javascript Password Generator designed to use uppercase, lowercase, numbers and special characters arrays to string together a PW 
+that is a minimum of 8 to a maximum of 128 characters. Using Arrays, concat, and several booleans confirmations a security phrase is generated for the user. I had intially tried constructing this project using string length variables, and while it functioned, I found the code to be extremely bloated, and decided to use Arrays instead, letting JS do the work for me once, as opposed to every single time I called the variable in a 'for' loop. 
+
+I have added a few other elements to the page, such as alerts informing the user of selection requirements and sending them back through. Improvments I'd like to add later on are better function loops that only return to the appropriate user prompts. 
+
+## Installation
+
+You can run this program through VS Code, or your chosen code editor
+## Usage
+
+Afer running the index.HTML in your browser(I use Chrome personally), open up the console with Shift+Ctrl+I and then click the Generate password button. Follow the prompts and check in console to see the magic.
+
+
+    ```md
+    ![home page](Assets\Javascript-Main.png)
+    ```
+    ```md
+    ![first prompt](Assets\First Prompt.png)
+
+## Credits
+(n.d.). ParseInt(). Mdn Webdocs. Retrieved May 15, 2023, from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+
+(n.d.). Java Array Loops. W3 Schools. Retrieved May 15, 2023, from https://www.w3schools.com/java/java_arrays_loop.asp
+
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Cody Cooke]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
